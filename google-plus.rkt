2355cb9d6d19f2b75b3ed7057f7d420999fa763f
Greg Hendershott <greghendershott@gmail.com>
2012-09-28 18:06:27 -0400

    Just regroup code lines

--- google-plus.rkt
+++ google-plus.rkt
@@ -2,10 +2,7 @@
 
 (require wffi/client)
 
-(define endpoint (make-parameter "https://www.googleapis.com"))
-
-(define lib (wffi-lib "google-plus.md"))
-(define get-person (wffi-kwd-proc lib "Get person" endpoint))
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 
 (define (read-api-key [file (build-path (find-system-path 'home-dir)
                                         ".google-api-key")])
@@ -15,6 +12,10 @@
 (define api-key (make-parameter (read-api-key)))
 
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+
+(define endpoint (make-parameter "https://www.googleapis.com"))
+(define lib (wffi-lib "google-plus.md"))
+(define get-person (wffi-kwd-proc lib "Get person" endpoint))
 
 (get-person #:Host "www.googleapis.com"
             #:key (api-key)
