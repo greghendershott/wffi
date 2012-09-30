73cd8610da64798dc33fc6d3485708331b0071e3
Greg Hendershott <greghendershott@gmail.com>
2012-09-30 19:48:53 -0400

    More concise FFI

--- google-plus.rkt
+++ google-plus.rkt
@@ -3,16 +3,12 @@
 (require wffi/client
          json)
 
-;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
-
 (define (read-api-key [file (build-path (find-system-path 'home-dir)
                                         ".google-api-key")])
   (match (file->string file #:mode 'text)
     [(regexp "^\\s*(.*?)\\s*(?:[\r\n]*)$" (list _ k)) k]
     [else (error 'read-api-key "Bad format for ~a" file)]))
 (define api-key (make-parameter (read-api-key)))
-
-;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 
 ;; A helper to take the response dict and check the status code. If
 ;; 200, convert the bytes to a jsexpr. Else raise an error.
@@ -22,54 +18,57 @@
         [else (error who "HTTP Status ~a ~s\n~a"
                      code (dict-ref d 'HTTP-Text) (dict-ref d 'entity))]))
 
+
+(define (add-common-parameters h)
+  (hash-set* h
+             'key (api-key)
+             'pretty-print "false"))
+
 ;; When dealing with JSON, often need to do nested hash-refs. Analgesic:
 (define (dict-refs d . ks)
   (for/fold ([d d])
             ([k ks])
     (dict-ref d k)))
 
-;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
-
 ;; TO-DO: Make paginated versions of these which take nextPageToken from the
 ;; response and supply it as &pageToken, until done.
 
 (define endpoint (make-parameter "https://www.googleapis.com"))
 (define lib (wffi-lib "google-plus.md"))
-(define get-person (compose1 (lambda (x) (check-response 'get-person x))
-                             (wffi-rest-proc lib "Get person" endpoint)))
-(define search-people (compose1 (lambda (x) (check-response 'search-people x))
-                                (wffi-rest-proc lib "Search people" endpoint)))
-(define people-activity (compose1 (lambda (x) (check-response 'people-activity x))
-                                  (wffi-rest-proc lib "People activity" endpoint)))
-(define activity-list (compose1 (lambda (x) (check-response 'activity-list x))
-                                (wffi-rest-proc lib "Activity list" endpoint)))
+
+(define (make-proc name who)
+  (compose1 (lambda (x) (check-response who x))
+            (wffi-dict-proc lib name endpoint)
+            add-common-parameters
+            hash))
+
+(define-syntax-rule (defproc name api-name)
+  (begin (define name (make-proc api-name #'name))
+         (provide name)))
+
+(defproc get-person "Get person")
+(defproc search-people "Search people")
+(defproc people-activity "People activity")
+(defproc activity-list "Activity list")
+
+;; examples:
 
 #;
-(get-person 'key (api-key)
-            'prettyPrint "false"
-            'userId "107023078912536369392")
+(get-person 'userId "107023078912536369392")
 #;
-(search-people 'key (api-key)
-               'query "John McCarthy")
-#;
-(people-activity 'key (api-key))
-
+(search-people 'query "John McCarthy")
 
 (define (show-post-activity user-id)
   (define js
-    (activity-list 'key (api-key)
-                   'prettyPrint "false"
-                   'userId user-id
+    (activity-list 'userId user-id
                    'collection "public"))
   (define activities (dict-ref js 'items))
   (for/list ([a activities])
     (define activity-id (dict-ref a 'id))
     (define title (dict-ref a 'title))
-    (define plus-oners (people-activity 'key (api-key)
-                                        'activityId activity-id
+    (define plus-oners (people-activity 'activityId activity-id
                                         'collection "plusoners"))
-    (define resharers (people-activity 'key (api-key)
-                                       'activityId activity-id
+    (define resharers (people-activity 'activityId activity-id
                                        'collection "resharers"))
     (list activity-id
           title
