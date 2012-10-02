3d036cecfe75f5d03c49c652d7beee35884152ca
Greg Hendershott <greghendershott@gmail.com>
2012-10-02 16:16:52 -0400

    Flesh out more of the last.fm non-authenticated API

--- last.fm.md
+++ last.fm.md
@@ -1,4 +1,4 @@
-# last.fm
+# last.fm non-authenticated API
 
 See <http://www.last.fm/api/intro>.
 
@@ -27,3 +27,73 @@ GET /2.0/
 - `chart.getTopTags`
 - `chart.getTopTracks`
 
+# Artist
+
+## Request
+````
+GET /2.0/
+    ?method={}
+    &artist={}
+    &[mbid={}]
+    &api_key={api-key}
+    &format=json
+    &[limit=50]
+    &[page=1]
+    &[autocorrect={}]
+    &[festivalsonly={}]
+````
+
+ `method` can be:
+- `artist.getEvents`
+- `aritst.getInfo`
+- `artist.getPastEvents`
+- `artist.getPodcast`
+- `artist.getShouts`
+- `artist.getSimilar`
+- `artist.getTags`
+- `artist.getTopAlbums`
+- `artist.getTopFans`
+- `artist.getTopTags`
+- `artist.getTopTracks`
+
+# Album
+
+## Request
+````
+GET /2.0/
+    ?format=json
+    &method={}
+    &artist={}
+    &album={}
+    &[mbid={}]
+    &[country={}]
+    &api_key={api-key}
+    &[limit=50]
+    &[page=1]
+    &[autocorrect={}]
+````
+
+ `method` can be:
+- `album.getBuyLinks`
+- `album.getInfo`
+- `album.getShouts`
+- `album.getTags`
+- `album.getTopTags`
+
+# Event
+
+## Request
+````
+GET /2.0/
+    ?format=json
+    &method={}
+    &event={}
+    &api_key={api-key}
+    &[limit=50]
+    &[page=1]
+````
+
+ `method` can be:
+- `event.getAttendees`
+- `event.getInfo`
+- `event.getShouts`
--- last.fm.rkt
+++ last.fm.rkt
@@ -3,16 +3,12 @@
 (require wffi/client
          json)
 
-;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
-
 (define (read-api-key [file (build-path (find-system-path 'home-dir)
                                         ".last.fm-api-key")])
   (match (file->string file #:mode 'text)
     [(pregexp "^\\s*API Key\\s*=\\s*(.*?)\\s*\n+?" (list _ k)) k]
     [else (error 'read-api-key "Bad format for ~a" file)]))
 (define api-key (make-parameter (read-api-key)))
-
-;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 
 ;; A helper to take the response dict and check the status code. If
 ;; 200, convert the bytes to a jsexpr. Else raise an error.
@@ -22,25 +18,70 @@
         [else (error who "HTTP Status ~a ~s\n~a"
                      code (dict-ref d 'HTTP-Text) (dict-ref d 'entity))]))
 
-;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+(define (add-common-parameters h)
+  (hash-set* h
+             'api-key (api-key)))
+
+;; When dealing with JSON, often need to do nested hash-refs. Analgesic:
+(define (dict-refs d . ks)
+  (for/fold ([d d])
+            ([k ks])
+    (dict-ref d k)))
 
 (define endpoint (make-parameter "http://ws.audioscrobbler.com"))
 (define lib (wffi-lib "last.fm.md"))
 
+(define (chain . fs)
+  (apply compose1 (reverse fs)))
+
 (define-syntax-rule (defproc name api-name)
-  (begin (define name (compose1 (lambda (x) (check-response #'name x))
-                                (wffi-rest-proc lib api-name endpoint)))
+  (begin (define name (chain hash
+                             add-common-parameters
+                             (wffi-dict-proc lib api-name endpoint)
+                             (lambda (x) (check-response (syntax-e #'name) x))))
          (provide name)))
 
+;; last.fm exposes a small number of "objects", each at their own
+;; resource, with a `method` query parameters. (As opposed to each
+;; object-and-method being a distinct resource.)  Here we just expose
+;; each "object". It might be a nicer interface if we made a wrapper
+;; function for each distinct object-and-method, e.g. artist-events.
+
 (defproc chart "Chart")
+(defproc artist "Artist")
+(defproc album "Album")
+(defproc event "Event")
 
 ;; Examples
 #|
-(chart 'api-key (api-key)
-       'method "chart.getHypedArtists"
+
+(chart 'method "chart.getHypedArtists"
        'limit 1)
 
-(chart 'api-key (api-key)
-       'method "chart.getLovedTracks"
+(chart 'method "chart.getLovedTracks"
        'limit 1)
+
+(artist 'method "artst.getEvents"
+        'artist "Lady Gaga"
+        'limit 1)
+
+(artist 'method "artist.getTopTracks"
+        'artist "Lady Gaga"
+        'limit 5)
+
+(let ([js (artist 'method "artist.getTopTracks"
+                  'artist "Lady Gaga"
+                  'limit 5)])
+  (for/list ([x (dict-refs js 'toptracks 'track)])
+    (list (dict-ref x 'name)
+          (dict-ref x 'url))))
+
+(album 'method "album.getInfo"
+       'artist "Pink Floyd"
+       'album "Dark Side of the Moon"
+       'limit 1)
+
+(event 'method "event.getInfo"
+       'event 328799)
+
 |#
