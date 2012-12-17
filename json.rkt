26642ad2b8e07e7de3a530c8a60664eeae1882c9
Greg Hendershott <greghendershott@gmail.com>
2012-12-17 14:40:08 -0500

    Update json.rkt for api and api-struct refactoring.
    Missed this in commit 6998e2a5f57f7139cad8ca3888d748ba79dde791

--- json.rkt
+++ json.rkt
@@ -3,11 +3,11 @@
 (require json
          "api.rkt")
 
-(provide api->jsexpr
-         jsexpr->api)
+(provide api-func->jsexpr
+         jsexpr->api-func)
 
-(define/contract (api->jsexpr a)
-  (api? . -> . jsexpr?)
+(define/contract (api-func->jsexpr a)
+  (api-func? . -> . jsexpr?)
   (define (->js x)
     (match x
       [(optional x) (hasheq 'optional (->js x))]
@@ -15,8 +15,7 @@
       [(constant x) (hasheq 'constant x)]
       [(variable x) (hasheq 'variable (symbol->string x))]
       [else x]))
-
-  (match-define (api name docs _ method path query head resp-head) a)
+  (match-define (api-func name docs _ method path query head resp-head) a)
   (hasheq 'name name
           'docs docs
           'request-method (symbol->string method)
@@ -25,8 +24,8 @@
           'request-head (map ->js head)
           'response-head (map ->js resp-head)))
            
-(define/contract (jsexpr->api j)
-  (jsexpr? . -> . api?)
+(define/contract (jsexpr->api-func j)
+  (jsexpr? . -> . api-func?)
   (define (->a x)
     (match x
       [(? string? x) x]
@@ -34,21 +33,20 @@
       [(hash-table ('constant x)) (constant x)]
       [(hash-table ('variable x)) (variable (string->symbol x))]
       [(hash-table (k v)) (keyval k (->a v))]))
-
-  (init-api (hash-ref j 'name)
-            (hash-ref j 'docs)
-            (string->symbol (hash-ref j 'request-method))
-            (map ->a (hash-ref j 'request-path))
-            (map ->a (hash-ref j 'request-query))
-            (map ->a (hash-ref j 'request-head))
-            (map ->a (hash-ref j 'response-head))
-            ))
+  (init-api-func (hash-ref j 'name)
+                 (hash-ref j 'docs)
+                 (string->symbol (hash-ref j 'request-method))
+                 (map ->a (hash-ref j 'request-path))
+                 (map ->a (hash-ref j 'request-query))
+                 (map ->a (hash-ref j 'request-head))
+                 (map ->a (hash-ref j 'response-head))
+                 ))
 
 (module+ test
   (require rackunit
            "markdown.rkt")
   (let ()
-    (define before (wffi-obj (wffi-lib "example.md") "Example GET API"))
-    (define js (api->jsexpr before))
-    (define after (jsexpr->api js))
+    (define before (wffi-obj (wffi-lib "examples/imgur.md") "Stats"))
+    (define js (api-func->jsexpr before))
+    (define after (jsexpr->api-func js))
     (check-equal? before after)))
